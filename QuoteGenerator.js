89d78e726b385a4e75bcea5c10d6127571d8ce43
KS-Late-World <89298600+KS-Late-World@users.noreply.github.com>
2022-02-05 12:19:57 +0100

    Add files via upload

---
+++ QuoteGenerator.js
@@ -0,0 +1,21 @@
+function Quote() {
+    var qouteArray = [
+        var quoteArray = [
+        "When you reach the end of your rope, tie a knot in it and hang on.- Franklin D.Roosevelt",
+
+        "Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead",
+        " Spread love everywhere you go.Let no one ever come to you without leaving happier. - Mother Teresa",
+        "When you reach the end of your rope, tie a knot in it and hang on. - Franklin D.Roosevelt",
+        "Always remember that you are absolutely unique.Just like everyone else. - Margaret Mead",
+        "Don't judge each day by the harvest you reap but by the seeds that you plant. -Robert Louis Stevenson",
+        "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+        "Tell me and I forget.Teach me and I remember.Involve me and I learn. - Benjamin Franklin",
+        "It is during our darkest moments that we must focus to see the light. - Aristotle",
+        "Whoever is happy will make others happy too. - Anne Frank",
+        "Do not go where the path may lead, go instead where there is no path and leave a trail. - Ralph Waldo Emerson"
+
+
+
+    ];
+    document.querySelector('#text').innerHTML=quoteArray[1];
+}
